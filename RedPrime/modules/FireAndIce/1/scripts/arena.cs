f7024a8ff867248c2c742462a0be9b765ca300d1
eyepatchParrot <peter.vansandt@gmail.com>
2013-09-08 13:38:00 +0000

    Added new map.

--- RedPrime/modules/FireAndIce/1/scripts/arena.cs
+++ RedPrime/modules/FireAndIce/1/scripts/arena.cs
@@ -6,22 +6,23 @@ function createArena()
 
 function createBackground()
 {
-	%bgRightX = $Game::ScreenWidth / 2.0;
-	%bgLeftX = %bgRightX - $Game::ScreenWidth;
-	%bgTopY = $Game::ScreenHeight / 2.0;
-	%bgLowY = %bgTopY - $Game::ScreenHeight;
+	// %bgRightX = $Game::ScreenWidth / 2.0;
+	// %bgLeftX = %bgRightX - $Game::ScreenWidth;
+	// %bgTopY = $Game::ScreenHeight / 2.0;
+	// %bgLowY = %bgTopY - $Game::ScreenHeight;
 	
-	createOneBackground( %bgLeftX SPC %bgLowY, "FireAndIce:map" );
-	createOneBackground( %bgLeftX SPC %bgTopY, "FireAndIce:map" );
-	createOneBackground( %bgRightX SPC %bgLowY, "FireAndIce:map" );
-	createOneBackground( %bgRightX SPC %bgTopY, "FireAndIce:map" );
+	// createOneBackground( %bgLeftX SPC %bgLowY, "FireAndIce:map" );
+	// createOneBackground( %bgLeftX SPC %bgTopY, "FireAndIce:map" );
+	// createOneBackground( %bgRightX SPC %bgLowY, "FireAndIce:map" );
+	// createOneBackground( %bgRightX SPC %bgTopY, "FireAndIce:map" );
+	createOneBackground( 0 SPC 0, $Game::ArenaWidth SPC $Game::ArenaHeight, "FireAndIce:map" );
 }
 
-function createOneBackground( %pos, %img)
+function createOneBackground( %pos, %size, %img)
 {
 	%bg = new Sprite();
 	%bg.setPosition(%pos);
-	%bg.setSize($Game::ScreenWidth SPC $Game::ScreenHeight);
+	%bg.setSize(%size);
 	%bg.setSceneLayer(5);
 	%bg.setImage( %img );
 	mainScene.add( %bg );
